d9b1ad377dd399654c68d71d941a033b486d7eed
Felipe Almeida <felipegoa@hotmail.com>
2021-09-24 14:55:12 -0300

    feat: add CategoryCard component and edit category, location modal

--- src/pages/shop/index.tsx
+++ src/pages/shop/index.tsx
@@ -1,94 +1,257 @@
 import DrawerLateral from "../../components/molecules/DrawerLateral";
-import { IoIosClose } from 'react-icons/io';
+import { IoIosClose } from "react-icons/io";
 
-import React, { useState } from 'react';
-import { Container, ModalContainer } from './styles'; 
+import React, { useState } from "react";
+import { Container, ModalContainer } from "./styles";
 
 import DescriptionCard from "../../components/molecules/DescriptionCard";
 import InfoCard from "../../components/molecules/InfoCard";
-import Modal from '../../components/molecules/Modal';
-import CustomModal from '../../components/molecules/CustomModal';
-import { Button } from '../../components/atoms/Button';
+import CustomModal from "../../components/molecules/CustomModal";
+import { Button } from "../../components/atoms/Button";
+import { Input } from "../../components/molecules/Input";
+import { FiSearch } from "react-icons/fi";
+import { CategoryCard } from "../../components/molecules/CategoryCard";
+import { IoCellular, IoFastFood } from 'react-icons/io5';
+import { HiOutlineLocationMarker } from 'react-icons/hi';
+import { BiBuildings, BiMapAlt } from 'react-icons/bi';
+import { FaRoad } from 'react-icons/fa';
 
 const shop = () => {
-  const [timeTableModal, setTimeTableModal] = useState(false)
+  const [timeTableModal, setTimeTableModal] = useState(false);
+  const [categoryModal, setCategoryModal] = useState(false);
+  const [locationModal, setLocationModal] = useState(false);
+
+  const [businessState, setBusinessState] = useState("");
+  const [businessCity, setBusinessCity] = useState("");
+  const [publicPlace, setPublicPlace] = useState("");
+  const [number, setNumer] = useState("");
+  const [district, setDistrict] = useState("");
+  const [cep, setCep] = useState("");
+
+  // Categorias
+  const [category, setCategory] = useState("");
+
+  // Modal de horarios
 
   function handleOpenTimeModal() {
-    setTimeTableModal(true)
+    setTimeTableModal(true);
   }
 
   function toggleTimeModal() {
     setTimeTableModal(!timeTableModal);
   }
 
-  function handleCancelOperation() {
-    handleOpenTimeModal();
+  // Modal de categorias
+
+  function handleOpenCategoryModal() {
+    setCategoryModal(true);
+  }
+
+  function toggleCategoryModal() {
+    setCategoryModal(!categoryModal);
+  }
+
+  // Modal de localização
+
+  function handleOpenLocationModal() {
+    setLocationModal(true);
+  }
+
+  function toggleLocationModal() {
+    setLocationModal(!locationModal);
   }
 
   return (
     <>
-    <Container>
-      <CustomModal buttons={false} setModalOpen={toggleTimeModal} modalVisible={timeTableModal}>
-        <ModalContainer>
-          <div className="exit-container">
-            <h1>Horário de funcionamento</h1>
-            <IoIosClose onClick={toggleTimeModal} size={36} color={"black"} />
-          </div>
-          <div className="timeTables-container">
-            <div className="left-container">
-              <div className="dates">
-                <p>Segunda:</p>
-                <input type="datetime" name="time" id="1" />
-                <input type="datetime" name="time" id="2" />
+      <Container>
+        <CustomModal
+          buttons={false}
+          setModalOpen={toggleTimeModal}
+          modalVisible={timeTableModal}
+        >
+          <ModalContainer>
+            <div className="exit-container">
+              <h1>Horário de funcionamento</h1>
+              <IoIosClose onClick={toggleTimeModal} size={36} color={"black"} />
+            </div>
+            <div className="timeTables-container">
+              <div className="left-container">
+                <div className="dates">
+                  <p>Segunda:</p>
+                  <input type="datetime" name="time" id="1" />
+                  <input type="datetime" name="time" id="2" />
+                </div>
+                <div className="dates">
+                  <p>Terça:</p>
+                  <input type="datetime" name="time" id="1" />
+                  <input type="datetime" name="time" id="2" />
+                </div>
+                <div className="dates">
+                  <p>Quarta:</p>
+                  <input type="datetime" name="time" id="1" />
+                  <input type="datetime" name="time" id="2" />
+                </div>
+                <div className="dates">
+                  <p>Quinta:</p>
+                  <input type="datetime" name="time" id="1" />
+                  <input type="datetime" name="time" id="2" />
+                </div>
               </div>
-              <div className="dates">
-                <p>Terça:</p>
-                <input type="datetime" name="time" id="1" />
-                <input type="datetime" name="time" id="2" />
-              </div>
-              <div className="dates">
-                <p>Quarta:</p>
-                <input type="datetime" name="time" id="1" />
-                <input type="datetime" name="time" id="2" />
-              </div>
-              <div className="dates">
-                <p>Quinta:</p>
-                <input type="datetime" name="time" id="1" />
-                <input type="datetime" name="time" id="2" />
+              <div className="right-container">
+                <div className="dates">
+                  <p>Sexta:</p>
+                  <input type="datetime" name="time" id="1" />
+                  <input type="datetime" name="time" id="2" />
+                </div>
+                <div className="dates">
+                  <p>Sabado:</p>
+                  <input type="datetime" name="time" id="1" />
+                  <input type="datetime" name="time" id="2" />
+                </div>
+                <div className="dates">
+                  <p>Domingo:</p>
+                  <input type="datetime" name="time" id="1" />
+                  <input type="datetime" name="time" id="2" />
+                </div>
               </div>
             </div>
-            <div className="right-container">
-              <div className="dates">
-                <p>Sexta:</p>
-                <input type="datetime" name="time" id="1" />
-                <input type="datetime" name="time" id="2" />
-              </div>
-              <div className="dates">
-                <p>Sabado:</p>
-                <input type="datetime" name="time" id="1" />
-                <input type="datetime" name="time" id="2" />
-              </div>
-              <div className="dates">
-                <p>Domingo:</p>
-                <input type="datetime" name="time" id="1" />
-                <input type="datetime" name="time" id="2" />
-              </div>
+            <div className="buttons-container">
+              <Button title="Confirmar" border={true}></Button>
             </div>
-          </div>
-          <div className="buttons-container">
-            <Button title="Confirmar" border={true}></Button>
-          </div>
-        </ModalContainer>
-      </CustomModal>
-      <DrawerLateral greenOption={1}/>
-      <div className="cards-area">
-        <div className="left-area">
-        
-        <DescriptionCard 
-            imgSrc="/images/coffe-place.png"
-            title="Café da Maria"
-            quantStar={5}
-            description="Café da Maria
+          </ModalContainer>
+        </CustomModal>
+        <CustomModal
+          buttons={false}
+          setModalOpen={toggleCategoryModal}
+          modalVisible={categoryModal}
+        >
+          <ModalContainer>
+            <div className="exit-container">
+              <h1>Categorias</h1>
+              <IoIosClose
+                onClick={toggleCategoryModal}
+                size={36}
+                color={"black"}
+              />
+            </div>
+            <div className="categories-container">
+              <Input
+                label=""
+                placeholder="Categoria"
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+                icon={<FiSearch size={20} color="var(--black-800)" />}
+              ></Input>
+            </div>
+            <div className="category-container">
+              <CategoryCard
+                label="Alimentação"
+                icon={<IoFastFood size={20} color="#3C8EFC" />}
+              />
+              <CategoryCard
+                label="Alimentação"
+                icon={<IoFastFood size={20} color="red" />}
+              />
+              <CategoryCard
+                label="Eletronicos"
+                icon={<IoCellular size={20} color="yellow" />}
+              />
+              <CategoryCard
+                label="Celulares"
+                icon={<IoCellular size={20} color="purple" />}
+              />
+            </div>
+            <div className="buttons-container">
+              <Button title="Confirmar" border={true}></Button>
+            </div>
+          </ModalContainer>
+        </CustomModal>
+        <CustomModal
+          buttons={false}
+          setModalOpen={toggleLocationModal}
+          modalVisible={locationModal}
+        >
+          <ModalContainer>
+            <div className="exit-container">
+              <h1>Localização</h1>
+              <IoIosClose
+                onClick={toggleLocationModal}
+                size={36}
+                color={"black"}
+              />
+            </div>
+            <div className="inputRow">
+              <Input
+                label="Estado"
+                placeholder="Estado"
+                value={businessState}
+                onChange={(e) => setBusinessState(e.target.value)}
+                icon={
+                  <HiOutlineLocationMarker size={20} color="var(--black-800)" />
+                }
+              />
+
+              <Input
+                label="Cidade"
+                placeholder="Cidade"
+                value={businessCity}
+                onChange={(e) => setBusinessCity(e.target.value)}
+                icon={
+                  <HiOutlineLocationMarker size={20} color="var(--black-800)" />
+                }
+              />
+            </div>
+            <div className="inputRow">
+              <Input
+                label="Logradouro"
+                placeholder="Logradouro"
+                flex={3}
+                value={publicPlace}
+                onChange={(e) => setPublicPlace(e.target.value)}
+                icon={<FaRoad size={20} color="var(--black-800)" />}
+              />
+
+              <Input
+                label="Número"
+                placeholder="0000"
+                value={number}
+                flex={1}
+                type="numeric"
+                maxLength={6}
+                onChange={(e) => setNumer(e.target.value)}
+                icon={<BiBuildings size={20} color="var(--black-800)" />}
+              />
+            </div>
+            <div className="last-inputRow">
+              <Input
+                label="Bairro"
+                placeholder="Bairro"
+                value={district}
+                onChange={(e) => setDistrict(e.target.value)}
+                icon={<BiMapAlt size={20} color="var(--black-800)" />}
+              />
+
+              <Input
+                label="CEP"
+                placeholder="000.000.000-00"
+                value={cep}
+                onChange={(e) => setCep(e.target.value)}
+                icon={<BiMapAlt size={20} color="var(--black-800)" />}
+              />
+            </div>
+            <div className="buttons-container">
+              <Button title="Confirmar" border={true}></Button>
+            </div>
+          </ModalContainer>
+        </CustomModal>
+        <DrawerLateral greenOption={1} />
+        <div className="cards-area">
+          <div className="left-area">
+            <DescriptionCard
+              imgSrc="/images/coffe-place.png"
+              title="Café da Maria"
+              quantStar={5}
+              description="Café da Maria
                 
                 Horário de funcionamento: 
                 Seg - Sex: 6h - 20h
@@ -96,53 +259,53 @@ const shop = () => {
                 
                 Os mais diversos tipos de petiscos para saborear com todos as nossas deliciosas receitas com café!
             "
-        />
+            />
 
-        <InfoCard 
-          title="Informações de Contato"
-          type="contact"
-          cell="(00)0000-0000"
-          facebook="facebook.com/exemplo"
-          linkedin="linkedin.com/exemplo"
-          whatsApp="wa.me/5000000000"
-        />
-        
+            <InfoCard
+              title="Informações de Contato"
+              type="contact"
+              cell="(00)0000-0000"
+              facebook="facebook.com/exemplo"
+              linkedin="linkedin.com/exemplo"
+              whatsApp="wa.me/5000000000"
+            />
 
-        <button className="config-button"> Configurações Adicionais </button>
+            <button className="config-button">
+              {" "}
+              Configurações Adicionais{" "}
+            </button>
+          </div>
 
+          <div className="right-area">
+            <InfoCard
+              title="Horário de funcionamento"
+              type="timetable"
+              button={() => handleOpenTimeModal()}
+              dom={["7:00", "12:00"]}
+              seg={["7:00", "12:00"]}
+              ter={["7:00", "12:00"]}
+              qua={["7:00", "12:00"]}
+              qui={["7:00", "12:00"]}
+              sex={["7:00", "12:00"]}
+              sab={["7:00", "12:00"]}
+            />
+
+            <InfoCard
+              title="Categorias"
+              type="category"
+              button={() => handleOpenCategoryModal()}
+              category="Alimentação"
+            />
+
+            <InfoCard
+              title="Localização"
+              type="local"
+              button={() => handleOpenLocationModal()}
+              local="Avenida Paulista, 63892, São Paulo - SP, 000.000-000"
+            />
+          </div>
         </div>
-
-        <div className="right-area">
-          <InfoCard 
-            title="Horário de funcionamento"
-            type="timetable"
-            button={() => handleOpenTimeModal()}
-            dom={["7:00","12:00"]}
-            seg={["7:00","12:00"]}
-            ter={["7:00","12:00"]}
-            qua={["7:00","12:00"]}
-            qui={["7:00","12:00"]}
-            sex={["7:00","12:00"]}
-            sab={["7:00","12:00"]}
-          />
-
-          <InfoCard 
-            title="Categorias"
-            type="category"
-            category="Alimentação"
-          />
-
-          <InfoCard 
-            title="Localização"
-            type="local"
-            local="Avenida Paulista, 63892, São Paulo - SP, 000.000-000"
-          />
-        </div>
-
-      </div>
-
-    </Container> 
-
+      </Container>
     </>
   );
 };
