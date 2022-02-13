abba646ed7762423139532fffaeee61ada69dcc2
mikael-r <mikarg9@gmail.com>
2022-02-12 22:47:30 -0300

    fix: marker, zoom and position

--- src/components/molecules/MapBdv/index.tsx
+++ src/components/molecules/MapBdv/index.tsx
@@ -1,20 +1,24 @@
 import Map from 'components/atoms/Map'
 
 const MapBdv = () => {
-  const DEFAULT_CENTER = [38.907132, -77.036546]
+  const DEFAULT_CENTER = [-23.565985644182255, -46.65077920923577]
 
   return (
     <Map
-      style={{ width: '90%', height: '400px' }}
+      style={{ width: '70%', height: '400px', borderRadius: '42px' }}
       center={DEFAULT_CENTER}
-      zoom={12}
+      minZoom={14}
+      zoom={22}
+      zoomControl={false}
+      attributionControl={false}
     >
-      {({ TileLayer }) => (
+      {({ TileLayer, Marker }) => (
         <>
           <TileLayer
             id="mapbox/streets-v11"
             url="https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw"
           />
+          <Marker position={DEFAULT_CENTER} />
         </>
       )}
     </Map>
