2c4a94ee92710660441a9fcd5bb5c1db3a1d2b4e
mikael-r <mikarg9@gmail.com>
2022-02-13 14:34:07 -0300

    feat: create footer landing

---
+++ src/components/molecules/FooterLanding/index.tsx
@@ -0,0 +1,40 @@
+import MapBdv from 'components/molecules/MapBdv'
+
+import { Footer } from 'styles/pages/Product'
+import { ContainerTerms } from './styles'
+
+import { AiFillPhone, AiOutlineWhatsApp } from 'react-icons/ai'
+
+const FooterLanding = () => {
+  return (
+    <Footer>
+      <div>
+        <h1>Boa de Venda</h1>
+
+        <span>CNPJ: 26.745.054/0001-70</span>
+        <span>Avenida Paulista, 63892, São Paulo - SP</span>
+
+        <h1>Contato</h1>
+
+        <span>
+          <AiFillPhone size={24} color="var(--gray-700)" />
+          +55 (86) 9 8178-9622
+        </span>
+
+        <span>
+          <AiOutlineWhatsApp size={24} color="var(--gray-700)" />
+          Whatsapp
+        </span>
+
+        <ContainerTerms>
+          <span>Termos de Uso e Políticas de Privacidade</span>
+          <span>Copyright &#169; 2021 | Sino – Marketing &#38; Tecnologia</span>
+        </ContainerTerms>
+      </div>
+
+      <MapBdv />
+    </Footer>
+  )
+}
+
+export default FooterLanding
---
+++ src/components/molecules/FooterLanding/styles.ts
@@ -0,0 +1,5 @@
+import styled from 'styled-components'
+
+export const ContainerTerms = styled.div`
+  padding-top: var(--spacing-xs);
+`
