4b3c4bb0ac187ba338bc09361d7ac898d0793b1e
Leonardo Lima <leonardo.b.lima1@gmail.com>
2022-02-25 11:03:51 -0300

    feat: adding function dependecy in categorcard

--- src/components/molecules/CategoryCard/index.tsx
+++ src/components/molecules/CategoryCard/index.tsx
@@ -4,11 +4,13 @@ import { IoIosClose } from 'react-icons/io'
 
 interface CategoryCard extends React.AllHTMLAttributes<HTMLDivElement> {
   label: string
+  click?: () => void
   // icon: ReactElement;
 }
 
 export const CategoryCard = ({
-  label
+  label,
+  click
 }: // icon
 CategoryCard) => {
   return (
@@ -18,7 +20,7 @@ CategoryCard) => {
         <div className="label">{label}</div>
         <div className="close-btn">
           <div className="wrap"></div>
-          <IoIosClose size={30} color={'#363F4E'} />
+          <IoIosClose size={30} color={'#363F4E'} onClick={click} />
         </div>
       </div>
     </Container>
