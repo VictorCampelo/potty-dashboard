c9f6e1cf1dc7b898d1d522a8eb05d094735682a6
Leonardo Lima <leonardo.b.lima1@gmail.com>
2022-03-03 15:23:39 -0300

    feat: adding paymentContext

--- src/components/atoms/PaymentItem/index.tsx
+++ src/components/atoms/PaymentItem/index.tsx
@@ -1,22 +1,30 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { Container } from './style'
+import { PaymentContext } from '../../../contexts/PaymentContext'
 
-interface IPaymentProps {
+interface IPaymentItemProps {
   label: string
-  value?: string
+  value: string
 }
 
-export const PaymentItem = (props: IPaymentProps) => {
+export const PaymentItem = (props: IPaymentItemProps) => {
   const [isChecked, setIsChecked] = useState(false)
+  const { setInputPaymentValue, inputPaymentValue } = useContext(PaymentContext)
+
+  function handleChange() {
+    setIsChecked(!isChecked)
+    if (!isChecked) {
+      setInputPaymentValue([props.value, ...inputPaymentValue])
+      return
+    }
+    setInputPaymentValue(
+      inputPaymentValue.filter((item) => item != props.value)
+    )
+  }
 
   return (
     <Container>
-      <input
-        type="checkbox"
-        checked={isChecked}
-        onChange={() => setIsChecked(!isChecked)}
-        value={props.value}
-      />
+      <input type="checkbox" checked={isChecked} onChange={handleChange} />
       {props.label}
     </Container>
   )
--- src/pages/dashboard/loja/index.tsx
+++ src/pages/dashboard/loja/index.tsx
@@ -1,10 +1,11 @@
 import DrawerLateral from '../../../components/molecules/DrawerLateral'
 import DrawerBottom from '../../../components/molecules/DrawerBottom'
 import { IoIosClose } from 'react-icons/io'
+import { PaymentContext } from '../../../contexts/PaymentContext'
 
 import { MultiSelect as MyMultSelect } from 'react-multi-select-component'
 
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useContext, useState } from 'react'
 import {
   ConfigButton,
   Container,
@@ -439,19 +440,11 @@ const Shop = ({ storeId, id }: Shop) => {
 
   // MODAL METHODS
   const [modalPayIsOpen, setModalPayIsOpen] = useState(false)
-  const [teste, setTeste] = useState({})
+  const { inputPaymentValue } = useContext(PaymentContext)
 
   function handleChangeModalOpen(funcModalClose, funcModalOpen) {
     funcModalClose(true)
     funcModalOpen(false)
-  }
-
-  function abc(data) {
-    setTeste(data)
-  }
-
-  function abcd() {
-    console.log(teste)
   }
 
   const methodsOfPaymentsFake = [
@@ -1156,7 +1149,7 @@ const Shop = ({ storeId, id }: Shop) => {
 
             <div className="payment-options">
               {methodsOfPaymentsFake.map((item, i) => (
-                <PaymentItem key={i} label={item} />
+                <PaymentItem key={i} label={item} value={item} />
               ))}
             </div>
 
@@ -1171,7 +1164,10 @@ const Shop = ({ storeId, id }: Shop) => {
                 />
               </div>
               <div className="wrap-btn-confirm">
-                <ConfigButton title="Confirmar" />
+                <ConfigButton
+                  title="Confirmar"
+                  onClick={() => console.log(inputPaymentValue)}
+                />
               </div>
             </div>
           </ModalContainer>
